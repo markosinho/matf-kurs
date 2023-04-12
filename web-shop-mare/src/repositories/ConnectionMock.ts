072b29e0bb774834a6b5df3e9a32e6070df069fd
Marko Stanković <markosinjo@gmail.com>
2023-04-12 09:15:54 +0200

    Mock Mongo fix for saving and deleting.

--- web-shop-mare/src/repositories/ConnectionMock.ts
+++ web-shop-mare/src/repositories/ConnectionMock.ts
@@ -19,10 +19,32 @@ export class MongoManagerMock {
     save(obj: any) {
 
         if (obj.hasOwnProperty('email'))
-            this._databaseMock['UserEntity'].push(obj);
+            this._save(obj, 'UserEntity');
 
         if (obj.hasOwnProperty('codename'))
-            this._databaseMock['ProductEntity'].push(obj);
+            this._save(obj, 'ProductEntity');
+    }
+
+    _save(obj: any, entityType: string) {
+        if (entityType === 'UserEntity') {
+            // @ts-ignore
+            let user = this._databaseMock['UserEntity'].find(item => item['email'] === obj['email'])
+            if (user) {
+                user = obj;
+            } else {
+                this._databaseMock['UserEntity'].push(obj);
+            }
+        }
+
+        if (entityType === 'ProductEntity') {
+            // @ts-ignore
+            let user = this._databaseMock['ProductEntity'].find(item => item['codename'] === obj['codename'])
+            if (user) {
+                user = obj;
+            } else {
+                this._databaseMock['ProductEntity'].push(obj);
+            }
+        }
     }
 
     findOne(obj: any, params: any) {
@@ -39,6 +61,17 @@ export class MongoManagerMock {
     }
 
     deleteOne(obj: any, params: object) {
+        if (Object.keys(params).length === 1) {
+
+            const key = Object.keys(params)[0];
+            // @ts-ignore
+            const value = params[key];
+
+            this._databaseMock[obj.name] =
+                this._databaseMock[obj.name].filter((item: { [x: string]: any; }) => item[key] !== value);
+            return true;
+        }
+
         return false;
     }
 }
